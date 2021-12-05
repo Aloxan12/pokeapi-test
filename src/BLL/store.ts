5d0bec724b73005fedf099d629b2e84ba35f8639
Aloxan12 <kvakvaaak@mail.ru>
2021-12-06 00:49:59 +0300

    test

--- src/BLL/store.ts
+++ src/BLL/store.ts
@@ -2,7 +2,6 @@ import {combineReducers, createStore, applyMiddleware} from "redux";
 import {mainReducer} from "./mainReducer";
 import thunk from "redux-thunk";
 
-
 const rootReducer = combineReducers({
     main: mainReducer,
 })
